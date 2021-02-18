d692e374405ed921511eca41416e96e2616ea21f
pradeep-git135 <pradeepcr135@gmail.com>
2021-02-18 11:45:36 +0530

    first commit

--- webdriver_io_practice/wdio.conf.js
+++ webdriver_io_practice/wdio.conf.js
@@ -154,7 +154,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec',
+    reporters: ['spec',['junit', {outputDir: './junitReports'}]
         // [HtmlReporter, {
         //     debug: true,
         //     outputDir: './reports/html-reports/',
